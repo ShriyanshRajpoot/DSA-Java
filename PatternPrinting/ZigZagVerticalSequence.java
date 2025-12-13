f6f654f9bdb1c2457e6b3488aa180c946c5ee7dc
ShriyanshRajpoot <shriyanshrajpoot720@gmail.com>
2025-12-13 16:35:35 +0530

    patterns

---
+++ PatternPrinting/ZigZagVerticalSequence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ZigZagVerticalSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            System.out.println(left);
+            if (left != right) {
+                System.out.println(right);
+            }
+            left++;
+            right--;
+            sc.close();
+        }
+    }
+}
